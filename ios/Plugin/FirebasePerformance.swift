9de0df86cca4fc949584025e39c049e179407cd6
Robin Genz <mail@robingenz.dev>
2021-03-27 18:09:26 +0100

    feat: add ios support (#7)

--- ios/Plugin/FirebasePerformance.swift
+++ ios/Plugin/FirebasePerformance.swift
@@ -1,7 +1,38 @@
 import Foundation
 
+import Firebase
+import FirebasePerformance
+
 @objc public class FirebasePerformance: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        return value
+    var traces: [String:Trace] = [:]
+    
+    override init() {
+        if (FirebaseApp.app() == nil) {
+            FirebaseApp.configure()
+        }
+    }
+    
+    @objc public func startTrace(_ traceName: String) {
+        let trace = Performance.startTrace(name: traceName)
+        self.traces[traceName] = trace
+    }
+    
+    @objc public func stopTrace(_ traceName: String) {
+        guard let trace = self.traces[traceName] else {
+            return
+        }
+        trace.stop()
+        self.traces.removeValue(forKey: traceName)
+    }
+    
+    @objc public func incrementMetric(_ traceName: String, _ metricName: String, _ incrementBy: Int) {
+        guard let trace = self.traces[traceName] else {
+            return
+        }
+        trace.incrementMetric(metricName, by: Int64(incrementBy))
+    }
+    
+    @objc public func getTraceByName(_ traceName: String) -> Trace? {
+        return self.traces[traceName]
     }
 }
