a6da8918c4096220d7355535eaebe0e4963714be
Robin Genz <mail@robingenz.dev>
2021-03-27 14:35:21 +0100

    wip

--- android/src/main/java/dev/robingenz/capacitor/firebaseperformance/FirebasePerformance.java
+++ android/src/main/java/dev/robingenz/capacitor/firebaseperformance/FirebasePerformance.java
@@ -1,8 +1,33 @@
 package dev.robingenz.capacitor.firebaseperformance;
 
-public class FirebasePerformance {
+import com.google.firebase.perf.metrics.Trace;
 
-    public String echo(String value) {
-        return value;
+import java.util.HashMap;
+
+public class FirebasePerformance {
+    private HashMap<String, Trace> traces = new HashMap<String, Trace>();
+
+    public void startTrace(String traceName) {
+        Trace trace = com.google.firebase.perf.FirebasePerformance.getInstance().newTrace("test_trace");
+        trace.start();
+        this.traces.put(traceName, trace);
+    }
+
+    public void stopTrace(String traceName) {
+        Trace trace = this.traces.get(traceName);
+        trace.stop();
+        this.traces.remove(traceName);
+    }
+
+    public void incrementMetric(String traceName, String metricName, Integer incrementBy) {
+        Trace trace = this.traces.get(traceName);
+        trace.incrementMetric(metricName, incrementBy);
+    }
+
+    public Trace getTraceByName(String traceName) {
+        if (this.traces.containsKey(traceName)) {
+            return this.traces.get(traceName);
+        }
+        return null;
     }
 }
